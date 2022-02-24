0ee0e3c6dcf41b08670d33383015fb895f782255
Zain AlDeen Fayod <bits/stdc++.h19/2/2020GITLAB>
2022-02-24 17:59:11 -0500

    Finishing Desktop version

--- src/pages/SkillsMe.vue
+++ src/pages/SkillsMe.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-
+    <div class="flex pl-5 pt-5 text-grey">GET: ZainAldeenFayod / Skills / Me</div>
+    <div class="flex pl-5 pt-5">
+      <h1 style="color: #ff6c37; font-size: 2em"> My Skills</h1>
+    </div>
+    <div class="max-w-6 pl-5 text-white text-left">
+      <h1  style="font-size: 2em">{</h1>
+    </div>
+    <div class="max-w-6 pl-12 mb-2 text-white text-left" style="font-size: 1em">
+      "Expert": <pre>
+              "Laravel Framework, PHP, MySql,
+              JS, Vue.js, Vuex,
+              HTML, CSS, Bootstrap, Json, Ajax, JQuery
+              Element UI, Tailwind, Quasar,
+              Management and Team, Leading, Decision making,
+              Quick Learner, Good Speaker",
+    </pre>
+    </div>
+    <div class="max-w-6 pl-12 mb-2 text-white text-left" style="font-size: 1em">
+      "Mid_Level": <pre class="text-left" >&nbsp;&nbsp;         "Algorithms
+              data Structures
+              Problem solving
+              Design patterns",</pre>
+    </div>
+    <div class="max-w-6 pl-12 mb-2 text-white text-left" style="font-size: 1em">
+      "Beginner": <pre>
+              NodeJs, Express, MongoDB, Mongoose
+    </pre>
+    </div>
+    <div class="max-w-6 pl-5 text-white text-left">
+      <h1  style="font-size: 2em">}</h1>
+    </div>
   </div>
 </template>
 
